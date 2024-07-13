aba9844235b7b1d905855136d7197bfa288ffc62
AndyofStateFarm <andrewjb97@yahoo.com>
2024-07-13 00:15:31 -0400

    Finishing touches

--- my-app/src/TaskGenerator.js
+++ my-app/src/TaskGenerator.js
@@ -18,7 +18,7 @@ const TaskGenerator = () => {
         setCurrentTask({ ...tasks[currentTaskIndex] });
     };
 
-    const motivationalMessages = [
+    const motivation = [
         "Your off to a great start!",
         "You can do it!",
         "Your almost done!",
@@ -26,13 +26,13 @@ const TaskGenerator = () => {
 
     return (
         <div>
-            
             <h3><FancyText title text={`Task ${currentTask.id}`} /></h3>
+            <img src={currentTask.image} alt={currentTask.name} />
             <FancyText title text={`Task ${currentTask.name}`} />
             <FancyText text={`Status: ${currentTask.isCompleted ? 'Completed ✔' : 'Pending'}`} />
             <button onClick={markTaskCompleted}>Mark as Completed</button>
             <button onClick={nextTask}>Next Task</button>
-            <FancyText text={motivationalMessages[currentTaskIndex % motivationalMessages.length]} />
+            <FancyText text={motivation[currentTaskIndex % motivation.length]} />
         </div>
     );
 };
--- my-app/src/task.js
+++ my-app/src/task.js
@@ -1,7 +1,7 @@
 const tasks = [
-    { id: 1, name: "Complete the Coding Project", isCompleted: false, image: `https://cdn.freebiesupply.com/logos/thumbs/2x/visual-studio-code-logo.png`},
-    { id: 2, name: "Review Peers Coding Projects and Grade ", isCompleted: false, image: ``},
-    { id: 3, name: "Watch Assigned Videos and Complete Video Quizzes", isCompleted: false, image: ``},
+    { id: 1, name: "Complete the Coding Project", isCompleted: false, image: `https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/128px-Visual_Studio_Code_1.35_icon.svg.png`},
+    { id: 2, name: "Review Peers Coding Projects and Grade ", isCompleted: false, image: `https://historyrewriter.com/wp-content/uploads/2014/07/peer-review.jpg`},
+    { id: 3, name: "Watch Assigned Videos and Complete Video Quizzes", isCompleted: false, image: `https://yt3.googleusercontent.com/ytc/AIdro_nykHmqq5M9GrqptE3RnVUqAUiXRGGl3tVvtePHaMOR4A=s160-c-k-c0x00ffffff-no-rj`},
 ];
 
 export default tasks;
